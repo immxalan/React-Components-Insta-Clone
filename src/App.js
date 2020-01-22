121bcd210945200743d51136e29456ac50247a20
Allen Do <ad@Allens-MacBook-Pro.local>
2020-01-22 01:16:48 -0800

    Finished MVP requirements

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import PostsPage from "./components/PostsContainer/PostsPage.js";
+import SearchBar from "./components/SearchBar/SearchBarContainer";
+import dummyData from"./dummy-data.js";
 // import the PostsPage and SearchBar and add them to the App
 // import dummydata
-
 const App = () => {
   // set up state for the dummy data and pass to your PostsPage
+  const [data] = useState(dummyData);
   return (
     <div className="App">
-      {/* Add components here  and pass props where appropriate */}
+      <SearchBar />
+      <PostsPage data={data} />
     </div>
   );
 };
-
 export default App;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,16 +1,40 @@
 //Complete the necessary code in this file
-import React from "react";
+import React, {useState} from "react";
+import CommentSection from "../CommentSection/CommentSectionContainer";
 import Post from "./Post";
+// import App from "../../App.js"
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = (props) => {
+  // const [postData] = useState(props.data);
+  // console.log(postData);
+  console.log(props);
+  //   const postArray= postData.map(element => (
+  // [`${element.username}`, `${element.thumbnailUrl}`, `${element.imageUrl}`, `${element.likes}`, `${element.timestamp}`]
+  // ))
+  // console.log(postArray);
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.data.map(data => (
+				<div className="post-tunnel" key={data.timestamp}>
+          <Post post={data} />
+				</div>
+        ))}
     </div>
   );
 };
 
 export default PostsPage;
 
+// key={`${element.username} ${element.thumbnailUrl} ${element.imageUrl} ${element.likes} ${element.timestamp} ${element.comments}`}
+// {element.username} - {element.thumbnailUrl} - {element.imageUrl} - {element.likes} - {element.timestamp} - {element.comments}
+// prop.data.map((element, index) => {
+//   return (
+//       <p key={`${element.username}_{anObjectMapped.thumbnailUrl}_{element.imageUrl}_{element.likes}_{element.timestamp}_{element.comments}`}>
+//           {element.username} - {element.thumbnailUrl} - {element.imageUrl} - {element.likes} - {element.timestamp} - {element.comments}
+//       </p>
+
+  // const postArray= props.data.map(element => (
+  // [`${element.username}`, `${element.thumbnailUrl}`, `${element.imageUrl}`, `${element.likes}`, `${element.timestamp}`]
+  // ))
